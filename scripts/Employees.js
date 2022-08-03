d772e8982ce4d5d5c026dd6e99acf4a9ac065d4b
Sefora Rodriguez <seforarodriguez13@gmail.com>
2022-08-02 21:22:08 -0400

    showing how many orders sold each employee

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,6 +1,7 @@
 import { getEmployees } from "./database.js"
-
+import { getOrders } from "./database.js"
 const employees = getEmployees()
+const orders = getOrders()
 
 export const Employees = () => {
     let html = "<ul>"
@@ -12,5 +13,39 @@ export const Employees = () => {
     html += "</ul>"
 
     return html
+}
+
+document.addEventListener(
+    'click',
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if(itemClicked.id.startsWith('employee')) {
+            const [, employeeID] = itemClicked.id.split('--')
+            for(let employee of employees) {
+                if(employee.id === parseInt(employeeID))
+                window.alert(getOrderCount(employeeID,employee.name))
+            }
+        }
+    }
+)
+
+
+
+
+//define a function that will take a employeeID parameter
+export const getOrderCount = (SelectedEmployeeID,employeeName) => {
+    //declare a starter point for the counter = 0
+    let counter = 0
+    let html = `${employeeName} `
+    //iterate through the orders
+    for(let order of orders) {
+        //if there is a match with the employeeID
+        if (order.employeeId === parseInt(SelectedEmployeeID))
+            //then add 1++ to the counter
+            counter ++
+    }
+    //outside of the forloop have the string interpolated
+    //${employee.name} sold ${counter} products
+   return html += `sold ${counter} products`
 }
 
--- scripts/Products.js
+++ scripts/Products.js
@@ -14,3 +14,31 @@ export const Products = () => {
     return html
 }
 
+document.addEventListener(
+    'click',
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if(itemClicked.id.startsWith('product')) {
+            const [,productID] = itemClicked.id.split('--')
+            //invoke function
+            window.alert(getProductPrice(productID))
+        }
+    }
+)
+
+//function with a parameter
+//should recieve a productID 
+const getProductPrice = (productIDselected) => {
+    let html =" "
+    //iterate through the products array of objects
+    for(const product of products) {
+        //if statement if productID matches the array of objects of products
+        if(product.id === parseInt(productIDselected)) {
+            //then return a string with the product selected.
+            html += `${product.name} costs $${product.price}`
+            //large coffee costs $
+        }
+    }
+    return html
+}
+
